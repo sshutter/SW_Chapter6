0c22265fcde369c9454c28ea605470465aaa97cb
shutter <6331335921@student.chula.ac.th>
2023-02-09 15:31:04 +0700

    connect with mongoDB

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const hospitals = require('./routes/hospitals');
+const connectDB = require('./config/db')
 
 
 // Load env vars
 dotenv.config({path: './config/config.env'});
 
+// Connect to database
+connectDB();
+
 const app=express();
+
+app.use(express.json());
 
 app.use('/api/v1/hospitals', hospitals);
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port ', PORT));
+const PORT = process.env.PORT || 3000
+const server = app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port ', PORT));
+
+process.on('unhandledRejection',(err,promise) => {
+    console.log(`Error: ${err.message}`);
+    // close server & exit process
+    server.close(() => process.exit(1));
+})
